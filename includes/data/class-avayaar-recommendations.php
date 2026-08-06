417c37396e38f73fb4ff15eb892488526d269c30
Qazal Esmaeili <gisellesmaeili@gmail.com>
2026-08-06 11:10:49 +0330

    removed questions CPT, added avayaar_questions data, recommendations, scoring and ajax

--- includes/data/class-avayaar-recommendations.php
+++ includes/data/class-avayaar-recommendations.php
@@ -11,27 +11,27 @@ class Avayaar_Recommendations {
     public static function get_rules() {
         return array(
             'rhythm_driven' => array(
-                'string' => array( 0 ), // TODO: گیتار، ویولن
-                'keys'   => array( 0 ), // TODO: پیانو
-                'perc'   => array( 0 ), // TODO: تنبک، دف
+                'string' => array( 209, 207, 190, 188, 169, 167, 165, 160 ), // TODO: گیتار، ویولن
+                'keys'   => array( 162 ), // TODO: پیانو
+                'perc'   => array( 224, 186, 184, 182, 180, 178 ), // TODO: تنبک، دف
                 'none'   => array( 0 ),
             ),
             'ear_driven' => array(
-                'string' => array( 0 ),
-                'keys'   => array( 0 ),
-                'perc'   => array( 0 ),
+                'string' => array( 209, 207, 190, 188, 169, 167, 165, 160 ),
+                'keys'   => array( 162 ),
+                'perc'   => array( 224, 186, 184, 182, 180, 178 ),
                 'none'   => array( 0 ),
             ),
             'balanced' => array(
-                'string' => array( 0 ),
-                'keys'   => array( 0 ),
-                'perc'   => array( 0 ),
+                'string' => array( 209, 207, 190, 188, 169, 167, 165, 160 ),
+                'keys'   => array( 162 ),
+                'perc'   => array( 224, 186, 184, 182, 180, 178 ),
                 'none'   => array( 0 ),
             ),
             'beginner_friendly' => array(
-                'string' => array( 0 ),
-                'keys'   => array( 0 ),
-                'perc'   => array( 0 ),
+                'string' => array( 209, 207, 190, 188, 169, 167, 165, 160 ),
+                'keys'   => array( 162 ),
+                'perc'   => array( 224, 186, 184, 182, 180, 178 ),
                 'none'   => array( 0 ),
             ),
         );
